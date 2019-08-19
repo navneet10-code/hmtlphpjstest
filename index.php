1931b79241cfbcc7e84e3fee1c4819bb379e3ca5
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-08-19 15:46:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,19 @@
 <html>
-<h1>Upload widget demo</h1>
-<a href="#" id="upload_widget_opener">Upload multiple images</a>
-<div class="feature_thumb"/>
+<button id="upload_widget" class="cloudinary-button">Upload files</button>
+</html>
+<script src="https://widget.cloudinary.com/v2.0/global/all.js" type="text/javascript"></script>  
 
-<script
-  src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
-  crossorigin="anonymous"></script>
-<script src="https://widget.cloudinary.com/v2.0/next/global/all.js"> 
-  
-  <script>
-    $("#upload_widget_opener").cloudinary_upload_widget(
-  {
-    cloud_name: "ddumb92d7",
-    upload_preset: "vqqpvdfw",
-    cropping: "server",
-    folder: "user_photos",
-    theme: "minimal",
-    stylesheet: ".drag_content { border: 4px solid red }",
-    multiple: false,
-    sources: ["local"],
-    button_class: "ud_button inline no_margin",
-    button_caption: "Browse",
-    thumbnails: ".feature_thumb",
-    thumbnail_transformation: { width: 100, height: 100, crop: "fit" },
-    resource_type: "image"
-  },
-  function(error, result) {
-    console.log(error, result);
+<script type="text/javascript">  
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'my_cloud_name', 
+  uploadPreset: 'my_preset'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+    }
   }
-);
-  </script>
-  
-  </html>
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+  }, false);
+</script>
