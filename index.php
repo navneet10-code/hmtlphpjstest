f9ef75ec854cd69306bb19ce0079d2aedf36f430
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-08-19 15:49:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 
 <script type="text/javascript">  
 var myWidget = cloudinary.createUploadWidget({
-  cloudName: 'my_cloud_name', 
-  uploadPreset: 'my_preset'}, (error, result) => { 
+  cloudName: 'ddumb92d7', 
+  uploadPreset: 'vqqpvdfw'}, (error, result) => { 
     if (!error && result && result.event === "success") { 
       console.log('Done! Here is the image info: ', result.info); 
     }
